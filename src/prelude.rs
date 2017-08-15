d6ea2386cf6b8dda560341e6a2100869b4e93df1
loehnert <christian.loehnert@ssi-schaefer.com>
2017-08-15 17:24:07 -0400

    added prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,8 +1,7 @@
-
 pub use router::Router;
 pub use error::HttpError;
 pub use handler::Handler;
 pub use request::Request;
 pub use response::Response;
-pub use server::{Server,ServerStopper};
-pub use traits::{FromRequest,FromRequestAsRef};
+pub use server::{Server, ServerStopper};
+pub use traits::{FromRequest, FromRequestAsRef};
